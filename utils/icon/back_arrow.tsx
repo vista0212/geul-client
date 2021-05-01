8c5b1ae61e5e1788c9eb2d42b17d362171cf6c8f
k1oo <kbaek1202@gmail.com>
2021-05-01 19:43:49 +0900

    icon 추가

---
+++ utils/icon/back_arrow.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BackArrow = () => {
+  return (
+    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M13.3167 18.3333H33.3334V21.6667H13.3167V26.6667L6.66671 20L13.3167 13.3333V18.3333Z" fill="#C4C4C4"/>
+    </svg>
+  );
+}
+
+export default BackArrow;

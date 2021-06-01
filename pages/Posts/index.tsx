8fffb4b9d7f09aeea3661f9be4870aa2f70cba63
k1oo <kbaek1202@gmail.com>
2021-06-01 23:42:22 +0900

    up: 서버와의 통신 준비

--- pages/Posts/index.tsx
+++ pages/Posts/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { PostsWrapper } from "@pages/Posts/style";
-import { IPost } from "../../typings/db";
-import PostCard from "../../components/PostCard";
+import { PostsWrapper } from '@pages/Posts/style';
+import { IPost } from '../../typings/db';
+import PostCard from '../../components/PostCard';
+import axios from 'axios';
 
 
 const Posts = () => {
-  const post: IPost = { id: 1, title: '123dfasdfjdasfklasjdfklasjdflkasdjflkasjdfkl124512521521512512512', thumbnail: '1234', created_at: '2020-12-12' }
+  const posts = axios.get('http://localhost:3030/api/posts')
+    .then(v => v)
+    .catch(e => {
+      console.log(e);
+      return e;
+    });
+  console.log(posts);
+  const post: IPost = {
+    id: 1,
+    title: '123dfasdfjdasfklasjdfklasjdflkasdjflkasjdfkl124512521521512512512',
+    thumbnail: '1234',
+    created_at: '2020-12-12',
+  };
   return (
-    <PostsWrapper >
+    <PostsWrapper>
       <PostCard post={post} />
       <PostCard post={post} />
       <PostCard post={post} />
@@ -23,7 +36,7 @@ const Posts = () => {
       <PostCard post={post} />
       <PostCard post={post} />
     </PostsWrapper>
-  )
-}
+  );
+};
 
 export default Posts;

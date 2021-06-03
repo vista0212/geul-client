ed31a02358d443e16f28291ea773f85ab06f7291
k1oo <kbaek1202@gmail.com>
2021-06-04 06:18:24 +0900

    블로그 글 리스트 서버 통신

--- pages/Posts/index.tsx
+++ pages/Posts/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PostsWrapper } from '@pages/Posts/style';
 import { IPost } from '../../typings/db';
 import PostCard from '../../components/PostCard';
@@ -6,35 +6,19 @@ import axios from 'axios';
 
 
 const Posts = () => {
-  const posts = axios.get('http://localhost:3030/api/posts')
-    .then(v => v)
-    .catch(e => {
-      console.log(e);
-      return e;
-    });
+  const [posts, setPosts] = useState<IPost[]>([]);
+  useEffect(() => {
+    axios.get<IPost[]>('http://localhost:3030/api/posts')
+      .then(({ data }) => {
+        console.log({ data });
+        setPosts(data);
+      })
+      .catch(e => console.log(e));
+  }, []);
   console.log(posts);
-  const post: IPost = {
-    id: 1,
-    title: '123dfasdfjdasfklasjdfklasjdflkasdjflkasjdfkl124512521521512512512',
-    thumbnail: '1234',
-    created_at: '2020-12-12',
-  };
   return (
     <PostsWrapper>
-      <PostCard post={post} />
-      <PostCard post={post} />
-      <PostCard post={post} />
-      <PostCard post={post} />
-      <PostCard post={post} />
-      <PostCard post={post} />
-      <PostCard post={post} />
-      <PostCard post={post} />
-      <PostCard post={post} />
-      <PostCard post={post} />
-      <PostCard post={post} />
-      <PostCard post={post} />
-      <PostCard post={post} />
-      <PostCard post={post} />
+      {posts.map(post => <PostCard key={post.id} post={post} />)}
     </PostsWrapper>
   );
 };

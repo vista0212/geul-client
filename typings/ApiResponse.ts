0b203127fe17476b2c99262e2ba474c89fd853f2
k1oo <kbaek1202@gmail.com>
2021-08-19 07:52:46 +0900

    feat: ApiResponse 클래스 생성

---
+++ typings/ApiResponse.ts
@@ -0,0 +1,30 @@
+export class ApiResponse<T> {
+
+  constructor(
+    private _success: boolean,
+    private _code: boolean,
+    private _data: T,
+    private _message: string,
+  ) {
+  }
+
+  get isSuccess(): boolean {
+    return this._success;
+  }
+
+  get success(): boolean {
+    return this._success;
+  }
+
+  get code(): boolean {
+    return this._code;
+  }
+
+  get data(): T {
+    return this._data;
+  }
+
+  get message(): string {
+    return this._message;
+  }
+}

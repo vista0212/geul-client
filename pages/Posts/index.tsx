e87a5c6e0568f6fb3c79c0e88e9776328ad735d2
k1oo <kbaek1202@gmail.com>
2021-08-19 07:53:11 +0900

    feat: Fetcher 클래스 사용해서 기존 API Request 방식 변경

--- pages/Posts/index.tsx
+++ pages/Posts/index.tsx
@@ -2,19 +2,31 @@ import React, { useEffect, useState } from 'react';
 import { PostsWrapper } from '@pages/Posts/style';
 import { IPost } from '../../typings/db';
 import PostCard from '../../components/PostCard';
-import axios from 'axios';
+import { Fetcher } from '@utils/Fetcher';
 
 
 const Posts = () => {
   const [posts, setPosts] = useState<IPost[]>([]);
+  const [error, setError] = useState('');
+
   useEffect(() => {
-    axios.get<IPost[]>('http://localhost:3030/api/posts')
-      .then(({ data }) => {
-        console.log({ data });
-        setPosts(data);
+    Fetcher.get<IPost[]>('http://localhost:3030/api/posts')
+      .then(response => {
+        if (!response.isSuccess) {
+          setError(response.message || '게시글 목록 조회에 실패했습니다.');
+          return;
+        }
+
+        setPosts(response.data);
       })
-      .catch(e => console.log(e));
+      .catch();
   }, []);
+
+  if (error) {
+    alert(error);
+    return <></>;
+  }
+
   return (
     <PostsWrapper>
       {posts.map(post => <PostCard key={post.id} post={post} />)}

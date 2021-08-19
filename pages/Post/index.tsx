903f8acbfb5d4e3077e3f52d2cf10b644469b96d
k1oo <kbaek1202@gmail.com>
2021-08-20 08:13:18 +0900

    feat: fetch 여부 확인하는 로직 추가

--- pages/Post/index.tsx
+++ pages/Post/index.tsx
@@ -1,36 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoBack, PostBody, PostHeader, PostTitle, PostWrapper } from '@pages/Post/style';
 import BackArrow from '@utils/icon/back_arrow';
 import Share from '@utils/icon/Share';
 import Comments from '../../components/Comments';
 import { IComment, IPost } from '../../typings/db';
-import { Redirect, useParams } from 'react-router';
-import { useQuery } from 'react-query';
-import axios from 'axios';
+import { useParams } from 'react-router';
+import { Fetcher } from '@utils/Fetcher';
 
 const Post = () => {
   const { id } = useParams<{ id: string | undefined }>();
-  const { data, isLoading, error, isFetched } = useQuery<{ data: IPost }, Error>('post', () =>
-    axios.get(`http://localhost:3030/api/posts/${id}`), {});
+  const [post, setPost] = useState<IPost | undefined>(undefined);
+  const [error, setError] = useState('');
+  const [isFetched, setIsFetched] = useState(false);
+  useEffect(() => {
+    setIsFetched(false);
+    setPost(undefined);
+    setError('');
+    const fetchData = async () => {
+      try {
+        const response = await Fetcher.get<IPost>(`http://localhost:3030/api/posts/${id}`);
+        if (!response.isSuccess) {
+          setError(response.message);
+          setIsFetched(true);
+          return;
+        }
 
-  if (isLoading) {
-    return <div>로딩중...</div>;
-  }
+        setPost(response.data);
+        setIsFetched(true);
+      } catch (e) {
+        setError(e.message);
+        setIsFetched(true);
+      }
+    };
 
-  if (!data) {
-    return <Redirect to="/" />;
-  }
-
-  const post = data.data;
-
-  // called
-  console.log({ post });
+    fetchData();
+  }, []);
 
   const comments: Array<IComment> = [
     { id: 1, nickname: '123', created_at: '2020.12.12', body: '안녕하세요 반갑습니다. 반갑고 반갑습니다'.repeat(100) },
     { id: 2, nickname: '123', created_at: '2020.12.12', body: '125125251' },
     { id: 3, nickname: '123', created_at: '2020.12.12', body: '125125251' },
     { id: 4, nickname: '123', created_at: '2020.12.12', body: '125125251' }];
+
+  console.log({ isFetched, error, post });
+
+  if (!isFetched) {
+    return <></>;
+  }
+
+  if (isFetched && (error || !post)) {
+    alert(error);
+    location.href = '/';
+  }
+
   return (
     <PostWrapper>
       <GoBack>

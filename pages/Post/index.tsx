1a7e9bceb8b4eb874d0b38906fa287c9539d97aa
k1oo <kbaek1202@gmail.com>
2021-08-23 08:57:05 +0900

    feat: useQuery 사용해서 요청 관리

--- pages/Post/index.tsx
+++ pages/Post/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { GoBack, PostBody, PostHeader, PostTitle, PostWrapper } from '@pages/Post/style';
 import BackArrow from '@utils/icon/back_arrow';
 import Share from '@utils/icon/Share';
@@ -6,35 +6,13 @@ import Comments from '../../components/Comments';
 import { IComment, IPost } from '../../typings/db';
 import { useParams } from 'react-router';
 import { Fetcher } from '@utils/Fetcher';
+import { useQuery } from 'react-query';
+import { ApiResponse } from '../../typings/ApiResponse';
 
 const Post = () => {
   const { id } = useParams<{ id: string | undefined }>();
-  const [post, setPost] = useState<IPost | undefined>(undefined);
-  const [error, setError] = useState('');
-  const [isFetched, setIsFetched] = useState(false);
-  useEffect(() => {
-    setIsFetched(false);
-    setPost(undefined);
-    setError('');
-    const fetchData = async () => {
-      try {
-        const response = await Fetcher.get<IPost>(`http://localhost:3030/api/posts/${id}`);
-        if (!response.isSuccess) {
-          setError(response.message);
-          setIsFetched(true);
-          return;
-        }
-
-        setPost(response.data);
-        setIsFetched(true);
-      } catch (e) {
-        setError(e.message);
-        setIsFetched(true);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const { isLoading, error, data } = useQuery<ApiResponse<IPost>>(
+    '', () => Fetcher.get<IPost>(`http://localhost:3030/api/posts/${id}`));
 
   const comments: Array<IComment> = [
     { id: 1, nickname: '123', created_at: '2020.12.12', body: '안녕하세요 반갑습니다. 반갑고 반갑습니다'.repeat(100) },
@@ -42,14 +20,13 @@ const Post = () => {
     { id: 3, nickname: '123', created_at: '2020.12.12', body: '125125251' },
     { id: 4, nickname: '123', created_at: '2020.12.12', body: '125125251' }];
 
-  console.log({ isFetched, error, post });
-
-  if (!isFetched) {
-    return <></>;
+  if (isLoading) {
+    return <span>로딩중</span>;
   }
 
-  if (isFetched && (error || !post)) {
-    alert(error);
+
+  if (error || !data?.isSuccess) {
+    alert(error || data?.message);
     location.href = '/';
   }
 
@@ -60,11 +37,11 @@ const Post = () => {
         <span>뒤로가기</span>
       </GoBack>
       <PostHeader>
-        <PostTitle>{post?.title}</PostTitle>
+        <PostTitle>{data?.data.title}</PostTitle>
         <Share />
       </PostHeader>
       <PostBody>
-        {post?.body}
+        {data?.data.body}
       </PostBody>
       <Comments comments={comments} />
     </PostWrapper>

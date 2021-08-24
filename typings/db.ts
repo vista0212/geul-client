446b53a8d54d965edc01f4e14625f3a77703f927
k1oo <kbaek1202@gmail.com>
2021-08-24 09:07:13 +0900

    feat: create BaseEntity interface

--- typings/db.ts
+++ typings/db.ts
@@ -1,17 +1,21 @@
-export interface IPost {
+interface BaseEntity {
   id: number;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string;
+}
+
+export interface IPost extends BaseEntity {
   title: string;
   body: string;
   isPublished: string;
-  createdAt: string;
   file: IFile;
   comments: Array<IComment>;
 }
 
-export interface IComment {
+export interface IComment  extends BaseEntity {
   id: number;
   name: string;
-  created_at: string;
   body: string;
 }
 

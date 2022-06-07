d8a86bbb1e362256fd3d9ab2b80200e031432acf
vista0212 <kbaek1202@gmail.com>
2022-06-08 08:48:59 +0900

    toQueryString 유틸 함수 추가

---
+++ utils/toQueryString.ts
@@ -0,0 +1,22 @@
+import { isArray } from '@utils/isArray';
+
+export function toQueryString(queryObject?: object): string {
+  if (!queryObject) {
+    return '';
+  }
+
+  return Object.entries(queryObject)
+    .map(([key, value]) => {
+      if (isArray(value)) {
+        value = value.join(',');
+      }
+
+      if (!value || !value.length) {
+        return;
+      }
+
+      return encodeURIComponent(key) + '=' + encodeURIComponent(value);
+    })
+    .filter((v) => v)
+    .join('&');
+}
